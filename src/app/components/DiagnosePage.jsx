2afb34309a56ae0abacdf03b72f7a72a2ffd60c1
Ivan Wang <ivanwang@augmentum.com.cn>
2015-10-29 18:05:10 +0800

    add image function

--- src/app/components/DiagnosePage.jsx
+++ src/app/components/DiagnosePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-let { Styles, TextField, FlatButton } = require('material-ui');
+let { Styles, TextField, FlatButton, Snackbar} = require('material-ui');
 let { Colors } = Styles;
 
 const DiagnosePage = React.createClass({
-	
-	render() {
+	getStyles() {
 		let sty = {
 			imageInput: {
 		        cursor: 'pointer',
@@ -25,38 +24,94 @@ const DiagnosePage = React.createClass({
 		      	fontWeight: "normal",
 		      	color: "red",
 		      },
+		      image: {
+		      	width: "150",
+		      	height: "200",
+		      	marginRight: "10",
+		      },
 		};
-
+		return sty;
+	},
+	getInitialState() {
+		return {
+			images: [], 
+			patentName: "james",
+		};
+	},
+	render() {
+		console.log("rendersss"); 
+		
+		let sty = this.getStyles();
 		return (
 			<div>
+				<div style={{float: "left", width: "400px"}}>
+					<TextField  
+						underlineStyle={{borderColor:Colors.green500}}
+						floatingLabelText="Patient Name" 
+						value={this.state.patentName} />
 
-			<TextField  
-				underlineStyle={{borderColor:Colors.green500}}
-				floatingLabelText="Patient Name" />
-
-			<TextField style={{display:'block'}}
-				floatingLabelText="Description"
-				underlineStyle={{borderColor:Colors.green500}}
-				rows={1} rowsMax={4} 
-				multiLine={true}/>
+					<TextField style={{display:'block'}}
+						floatingLabelText="Description"
+						underlineStyle={{borderColor:Colors.green500}}
+						rows={1} rowsMax={4} 
+						multiLine={true}/>
 
 
-			<FlatButton primary={true} label="Choose an Image">
-			    <input type="file" id="imageFiles" multiple
-			    	style={sty.imageInput} onChange="{this._handleFileSelect}"></input>
-			 </FlatButton>
+					<FlatButton primary={true} label="Choose an Image">
+					    <input type="file" ref="imageFiles"
+					    	style={sty.imageInput} multiple
+					    	onChange={this._handleFileSelect}></input>
+					 </FlatButton>
 
-			<TextField  style={{display:'block'}}
-				floatingLabelText="Description" 
-				underlineStyle={{borderColor:Colors.green500}}
-				rowsMax = {4} hintText="10-15 words"
-				multiLine={true}/>
+					 <div>
+					 	{this.state.images}
+					 </div>
+					 
 
+					<TextField  style={{display:'block'}}
+						floatingLabelText="Description" 
+						underlineStyle={{borderColor:Colors.green500}}
+						rowsMax = {4} hintText="10-15 words"
+						multiLine={true}/>
+
+					<Snackbar
+			            ref="snackbar"
+			            message="invalid image" />
+		        </div>
+		        <div style={{float: "left", "borderLeft": "2px solid #999999", height: "300px", width: "2px"}}>
+
+					
+			    </div>
+		        <div style={{float: "left"}}>
+		        	right
+		        </div>
 			</div>
 		);
 	},
-	handleFileSelect(evt) {
-		console.log(evt);
+	_handleFileSelect(evt) {
+		let sty = this.getStyles();
+		let f;
+		let images = this.state.images;
+		for (let i = 0; f = evt.target.files[i]; i++) {
+			if (!f.type.match('image.*')) {
+		        this.refs.snackbar.show();
+		    } else {
+		    	let reader = new FileReader();
+		    	let _this = this;
+                reader.onload = function (e) {
+                	 images.push(
+                    	<img style={sty.image} src={e.target.result} />
+                    );
+                	_this.setState({images: images});
+                };
+
+                reader.readAsDataURL(f);
+		    }
+		}
+		
+		
+		this.refs.imageFiles.value = "";
+		
 	},
 
 });
---
+++ src/app/components/DragItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+let { Styles, TextField, FlatButton, SelectField} = require('material-ui');
+
+export const DragItem = React.createClass({
+	getDragOptions() {
+		return [
+			{id: "1", name: "Option1"},
+			{id: "2", name: "Option2"},
+			{id: "3", name: "Option3"},
+		]
+	}
+	render() {
+		let options = getDragOptions();
+		return (
+			<div>
+				drug <TextField hintText={"this.prop.dragName"} /> g
+				<SelectField
+					  valueMember="id"
+					  displayMember="name"
+					  menuItems={options} />
+				
+			</div>
+		);
+	}
+});
